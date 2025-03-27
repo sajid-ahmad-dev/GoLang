0af9445ae7393104a7d9daf01472a6dd310cd063
Sajid Ahmad <sajid01ahmad@gmail.com>
2025-03-28 00:06:59 +0530

    Add 02_simple_values directory

---
+++ 02_simple_values/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main(){
+	// int 
+	fmt.Println(1)
+
+	// string
+	fmt.Println("Hello GoLang")
+
+	// float
+	fmt.Println(72.5)
+
+	// bool
+	fmt.Println(true)
+	fmt.Println(false)
+
+	// operations
+	fmt.Println(1+72.5)
+	fmt.Println(75.0/5)
+	fmt.Println(73.3/3)
+	
+}
